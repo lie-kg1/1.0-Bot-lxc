5768eb23ca21779b44b4f5ec07ac4b981f2b4c30
lie_kg <fetgeryrd0187@gmail.com>
2026-08-06 11:29:13 +0200

    Update 247.sh

--- 247.sh
+++ 247.sh
@@ -14,15 +14,15 @@ fi
 
 while true; do
     clear
-    echo "⚡ =========================================="
+    echo "=============================================="
     echo "                24/7 MANAGER                 "
     echo "============================================"
     echo ""
-    echo "1. 🚀 Start Bot (24/7 Background)"
-    echo "2. 🔄 Restart Bot"
-    echo "3. 🛑 Stop Bot"
-    echo "4. 📊 Live Status & Logs"
-    echo "5. ❌ Exit"
+    echo "1. 🚀  Start Bot (24/7 Background)"
+    echo "2. 🔄  Restart Bot"
+    echo "3. 🛑  Stop Bot"
+    echo "4. 📊  Live Status & Logs"
+    echo "5. ❌  Exit"
     echo "--------------------------------------------"
     read -p "Enter choice [1-5]: " choice
 
